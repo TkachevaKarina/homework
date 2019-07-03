4ffcfb4f078c35a33bf2c543bcfaca6fd35614e2
TkachevaKarina <tkachevakarin@mail.ru>
2019-07-03 16:46:34 +0300

    Main

--- Main.java
+++ Main.java
@@ -6,20 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        final int defaultCountPotatosInToPackage = 30;
-        System.out.println("Package capacity: "+defaultCountPotatosInToPackage);
 
-        System.out.print("Please enter count potatos: ");
+        System.out.print("Enter count potatos: ");
         int countPotatos=scanner.nextInt();
 
-        System.out.print("Please enter count package: ");
+        System.out.print("Enter count package: ");
         int countPackage=scanner.nextInt();
 
-        int countPotatosInToPackages=defaultCountPotatosInToPackage*countPackage;
-        if (countPotatosInToPackages > countPotatos){
-            System.out.println("Count Potatos in to package: " + countPotatos);
-        }else{
-            System.out.println("Count Potatos in to package: "+countPotatosInToPackages);}
-        System.out.println("Potato residue or (if negative meaning) empty bag space: "+(countPotatos-countPotatosInToPackages));
+        int countPotatosInPackages;
+        countPotatosInPackages = countPotatos/countPackage;
+
+        System.out.println("Count potatos in package:" + countPotatosInPackages);
+        System.out.println("Remainder potatos:" + (countPotatos - (countPackage * countPotatosInPackages)));
     }
 }
